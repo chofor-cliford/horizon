d24f9308bf4a97581fc6fa961de604ccac7690e9
Chofor <betterfuture032@gmail.com>
2024-06-05 18:45:35 -0600

    Fix Typescript error

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,7 +9,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const loggedIn = await getLoggedInUser();
+  const loggedIn:User = await getLoggedInUser();
 
   if (!loggedIn) redirect("/sign-in");
 
